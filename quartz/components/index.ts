df2432d2f6ca1c76ba31d016114e2b994f1caf62
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-08-24 20:46:21 -0400

    Deploy Website: 2026-08-24 08:46:21 PM

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -1,14 +1,32 @@
 import NotFound from "./pages/404"
+
 import Head from "./Head"
+
 import Spacer from "./Spacer"
+
 import DesktopOnly from "./DesktopOnly"
+
 import MobileOnly from "./MobileOnly"
+
 import Flex from "./Flex"
+
 import ConditionalRender from "./ConditionalRender"
-import Lightbox from "./scripts/lightbox"
+
+import Lightbox from "./Lightbox"
 
 export { componentRegistry, defineComponent } from "./registry"
+
 export { External } from "./external"
+
 export type { ComponentManifest, RegisteredComponent } from "./registry"
 
-export { Head, Spacer, DesktopOnly, MobileOnly, NotFound, Flex, ConditionalRender, Lightbox }
+export {
+  Head,
+  Spacer,
+  DesktopOnly,
+  MobileOnly,
+  NotFound,
+  Flex,
+  ConditionalRender,
+  Lightbox,
+}
--- quartz/components/lightbox.tsx
+++ quartz/components/lightbox.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
-
-import script from "./lightbox"
+import script from "./scripts/lightbox.inline"
 
 const Lightbox: QuartzComponent = () => {
   return null
