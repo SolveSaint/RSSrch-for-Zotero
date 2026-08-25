b6918d8df997b13c170fbe4f1c3e17316fda5121
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-08-24 20:07:23 -0400

    Deploy Website: 2026-08-24 08:07:23 PM

--- quartz/components/lightbox.tsx
+++ quartz/components/lightbox.tsx
@@ -1,6 +1,6 @@
-import script from "./scripts/lightbox"
-
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
+
+import script from "./scripts/lightbox"
 
 const Lightbox: QuartzComponent = () => {
   return null
